ad2513210cf161a80a8c6d7e439e33ab637ec854
atanda0x <atandanafiu21@gmail.com>
2023-05-14 23:14:34 +0100

    slack age bot is up and running

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/shomali11/slacker"
 )
@@ -21,27 +22,27 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 }
 
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5253855724966-5258361487205-YawGfJnAQGSacuh7Y1Zpbnzi")
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5253855724966-5258361487205-yqP9M6RYq9pYjcQOKQGTa1jW")
 	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A057GL291LN-5273929263185-0ae0de9d835e9391f38abcad559a531a850f3947b62eaa9ba46eb39c72985fd2")
 
 	bot := slacker.NewClient(os.Getenv("SLACKER_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
 	go printCommandEvents(bot.CommandEvents())
 
-	bot.Command("my yob is <year>", &slacker.CommandDefinition) {
-		Description: "yob calculator", 
-		Example: "my yob is 2023",
-		Handler: func (botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter)  {
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
+		Description: "yob calculator",
+
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			year := request.Param("year")
 			yob, err := strconv.Atoi(year)
 			if err != nil {
 				println("error")
 			}
-			age := 2022-yob
-			r :=  fmt.Sprintf("age is %d", age)
+			age := 2022 - yob
+			r := fmt.Sprintf("age is %d", age)
 			response.Reply(r)
-		}
-	}
+		},
+	})
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
